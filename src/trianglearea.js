a1e6c0053a37cafa7e37579737ae4d4c7562bedc
wleha1 <klerens1221@gmail.com>
2023-11-02 10:18:35 +0300

    src/trianglearea.js

---
+++ src/trianglearea.js
@@ -0,0 +1,8 @@
+/ Получаем данные от пользователя
+const a = parseFloat(prompt("Введите длину стороны a:"));
+const b = parseFloat(prompt("Введите длину стороны b:"));
+const c = parseFloat(prompt("Введите длину стороны c:"));
+// Рассчитываем полупериметр 
+const p = (a + b + c) / 2.0;
+// Рассчитываем площадь треуголника по формуле Генода
+console.log(`S = ` + Math.sqrt(p * (p - a) * (p - b) * (p - c)));
