670c0bd21e52663de3d1211e84e6c0983cd96946
wleha1 <klerens1221@gmail.com>
2023-11-02 09:57:02 +0300

    src/dialog.js

--- src/dialog.js
+++ src/dialog.js
@@ -1,4 +1,1 @@
-console.log('Jesse Pinkman: "Yo, Mr. White! I\'ve got a surprise for you!"');
-console.log('Walter White: "What is it?"');
-console.log('Jesse Pinkman: "It\'s a giant magnet!"');
-console.log('Walter White: "What the hell are we going to do with a giant magnet?"');
+console.log("Jesse Pinkman: Yo, Mr. White! I've got a surprise for you!\nWalter White: What is?\nJesse Pinkman: It's a giant magnet!\nWalter White: What the hell are we going to do with a giant magnet?");
