9128076ff2c616345b8a73109ed97bee1a0f9a65
wleha1 <klerens1221@gmail.com>
2023-11-03 10:28:17 +0300

    src/restaurant.js

---
+++ src/restaurant.js
@@ -0,0 +1,6 @@
+const allsum = 3500;
+const all = 500;
+const people = 8;
+const howmany = ((allsum + all)/people)
+
+console.log(`С кажого по ${howmany}, при общем счете ${allsum} и чаевыми ${all}`);
